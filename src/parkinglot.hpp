6e30403855398ac12104316f1f58a6080b10e639
Leo Chanthachith <leo.chanthachith1999@gmail.com>
2020-01-09 11:18:18 +0100

    complete parkinglot.hpp; added occupyLot() in parkinglot.cpp

---
+++ src/parkinglot.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <chrono>
+
+enum lot_type
+{
+    family,
+    electric,
+    small,
+    regular
+};
+
+class ParkingLot
+{
+private:
+    int lot_family;
+    int family_occupied;
+    int lot_electric;
+    int electric_occupied;
+    int lot_small;
+    int small_occupied;
+    int lot_regular;
+    int regular_occupied;
+
+public:
+    ParkingLot(int, int, int, int);
+    ~ParkingLot();
+    void occupyLot(lot_type);
+    void releaseLot(lot_type);
+    char * getCurrentOccupancy();
+};
